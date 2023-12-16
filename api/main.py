7ccaf2705baff2e8e814bceed4f7e9040c4c7396
Akriti Tiwari <113385222+akrititgit@users.noreply.github.com>
2023-12-17 00:03:40 +0530

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, File , UploadFile
+import uvicorn
+app=FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "hello"
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile=File(...)):
+    if __name__=="__main__":
+        uvicorn.run(app,host='localhost',port=8000)
